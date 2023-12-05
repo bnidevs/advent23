22a4fbc6c08417340e9b9adc97a5bc637de1a104
Bill Ni <bill.8ni@gmail.com>
2023-12-05 11:16:28 -0500

    3p2

--- 3/main.py
+++ 3/main.py
@@ -46,3 +46,52 @@ with open('input', 'r') as f:
 			tt += int(cl[k])
 
 	print(tt)
+
+with open('input', 'r') as f:
+	d = [x.strip() for x in f.readlines()]
+
+	r = 0
+	cl = {}
+	for l in d:
+
+		i = 0
+
+		cc = ''
+		s = set([str(x) for x in range(10)])
+		while i < len(l):
+			if l[i] in s:
+				cc += l[i]
+			elif len(cc) > 0:
+				cl[(r, i - len(cc))] = cc
+				cc = ''
+
+			i += 1
+
+		if len(cc) > 0:
+			cl[(r, i - len(cc))] = cc
+			cc = ''
+
+		r += 1
+
+	tt = 0
+
+	ng = {}
+
+	for k in cl:
+		au = [(k[0] - 1, k[1] + x) for x in range(-1, len(cl[k]) + 1)]
+		ab = [(k[0] + 1, k[1] + x) for x in range(-1, len(cl[k]) + 1)]
+
+		sr = au + ab + [( k[0], k[1] - 1 ), ( k[0], k[1] + len(cl[k]) )]
+
+		sr = [p for p in sr if 0 <= p[0] < len(d) and 0 <= p[1] < len(d[0])]
+
+
+		for p in sr:
+			if d[p[0]][p[1]] == '*':
+				if (p[0], p[1]) in ng:
+					tt += int(cl[k]) * ng[(p[0], p[1])]
+					del ng[(p[0], p[1])]
+				else:
+					ng[(p[0], p[1])] = int(cl[k])
+
+	print(tt)
